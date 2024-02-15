441a1bd56a7447074bce0e07c6818bf3dc970139
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-02-15 19:10:25 +0300

    changed Employee as added randomGenerationEmployee

--- src/main/java/ru/serjeypyzin/Employee.java
+++ src/main/java/ru/serjeypyzin/Employee.java
@@ -11,6 +11,8 @@ package ru.serjeypyzin;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Random;
+
 @Getter
 @Setter
 public class Employee {
@@ -26,6 +28,37 @@ public class Employee {
         this.employeeName = employeeName;
         this.phoneNumber = phoneNumber;
         this.workExperience = workExperience;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Сотрудник:\nТабельный номер: %d, Имя Фамилия: %s, телефон: %s, стаж: %.2f"
+                , serviceNumber
+                , employeeName
+                , phoneNumber
+                , workExperience);
+    }
+
+    public static Employee randomGenerationEmployee(){
+        Random random = new Random();
+
+        String[] firstNameArray = {"Иван", "Алексей", "Мария", "Елена", "Андрей"
+                , "Ольга", "Петр", "Светлана", "Наталья", "Дмитрий"};
+        String[] lastNameArray = {"Иванов", "Петров", "Сидоров", "Смирнов", "Козлов"
+                , "Морозов", "Новиков", "Кузнецова", "Павлова", "Волкова"};
+        String[] phoneNumbers = {"123456789", "987654321", "555555555", "111222333", "999888777"};
+        double minWorkExperience = 1.0;
+        double maxWorkExperience = 20.0;
+
+        String firstName = firstNameArray[random.nextInt(firstNameArray.length)];
+        String lastName = lastNameArray[random.nextInt(lastNameArray.length)];
+        String employeeName = String.format("%s %s",firstName, lastName);
+
+        String phoneNumber = phoneNumbers[random.nextInt(phoneNumbers.length)];
+
+        double workExperience = minWorkExperience + (maxWorkExperience - minWorkExperience) * random.nextDouble();
+
+        return new Employee(employeeName, phoneNumber, workExperience);
     }
 
 }
